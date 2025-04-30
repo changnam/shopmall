4a52c0f26bc6dc70ba03aebc90d3d40dd8c437b3
Changnam Go <changnam.go@gmail.com>
2025-04-30 12:15:06 +0900

    custom validator 에 messagesource 적용

--- src/main/java/com/honsoft/shopmall/validation/BookRequestValidator.java
+++ src/main/java/com/honsoft/shopmall/validation/BookRequestValidator.java
@@ -1,6 +1,8 @@
 package com.honsoft.shopmall.validation;
 import java.math.BigDecimal;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -10,6 +12,12 @@ import com.honsoft.shopmall.dto.BookRequest;
 @Component
 public class BookRequestValidator implements Validator {
 
+	private final MessageSource messageSource;
+	
+	public BookRequestValidator (MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
     @Override
     public boolean supports(Class<?> clazz) {
         return BookRequest.class.isAssignableFrom(clazz); // 해당 클래스만 검증
@@ -18,6 +26,7 @@ public class BookRequestValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
     	BookRequest book = (BookRequest) target;
+    	var locale = LocaleContextHolder.getLocale(); // 현재 요청 Locale
 
         if (book.name() == null || book.name().isBlank()) {
             errors.rejectValue("name", "required.name", "Name is required");
@@ -27,7 +36,8 @@ public class BookRequestValidator implements Validator {
         BigDecimal bd3 = new BigDecimal("500");
         
         if (book.unitPrice().compareTo(bd1) < 0 || book.unitPrice().compareTo(bd3) > 0) {
-            errors.rejectValue("unitPrice", "invalid.unitPrice", "unitPrice must be between 0 and 500");
+        	String msg = messageSource.getMessage("invalid.unitPrice", null, locale);
+            errors.rejectValue("unitPrice", "invalid.unitPrice", msg);
         }
     }
 }
