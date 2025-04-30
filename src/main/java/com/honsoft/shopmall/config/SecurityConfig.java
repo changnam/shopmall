4934195c8d89fadc2f016bb6aba7ad99611358f0
Changnam Go <changnam.go@gmail.com>
2025-05-01 08:51:28 +0900

    path에 권한 설정함. authorizationfilter 가 적용되고 anonymoussecuritycontext 가 만들어 졌으므로 exception 이 처리되는데, formlogin 설정이 안되었으므로 Http403ForbiddenEntryPoint 가 동작하고 결국 /error 로 전달되어 whitelable error page 가 보여짐

--- src/main/java/com/honsoft/shopmall/config/SecurityConfig.java
+++ src/main/java/com/honsoft/shopmall/config/SecurityConfig.java
@@ -11,31 +11,37 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SecurityConfig {
-	
+
 	@Value("${server.servlet.context-path:}")
-	private String contextPath; 
-	
+	private String contextPath;
+
 	@Bean
 	public SecurityFilterChain webSecurityConfig(HttpSecurity http) throws Exception {
 		String protectedPath = contextPath + "/**";
 		http.securityMatcher(protectedPath);
 		http.csrf((csrf) -> csrf.disable());
-		
+		http.authorizeHttpRequests((auth) -> auth.requestMatchers(contextPath + "/admin/**").hasAuthority("ROLE_ADMIN")
+				.requestMatchers(contextPath + "/manager/**").hasRole("MANAGER")
+				.requestMatchers(contextPath + "/member/**").authenticated()
+				.requestMatchers(contextPath + "/books/**").authenticated()
+				.anyRequest().permitAll());
+
 		return http.build();
 	}
-	
+
 	@Bean
 	public WebSecurityCustomizer webSecurity() {
-		
-		return (web) -> web.ignoring().requestMatchers(contextPath+"/css/**",contextPath+"/img/**",contextPath+"/js/**",contextPath+"/webjars/**");
+
+		return (web) -> web.ignoring().requestMatchers(contextPath + "/css/**", contextPath + "/img/**",
+				contextPath + "/js/**", contextPath + "/webjars/**");
 	}
-	
+
 	@Bean
 	public SecurityFilterChain apiSecurityConfig(HttpSecurity http) throws Exception {
 		String protectedPath = contextPath + "/api/**";
 		http.securityMatcher(protectedPath);
 		http.csrf((csrf) -> csrf.disable());
-		
+
 		return http.build();
 	}
 
