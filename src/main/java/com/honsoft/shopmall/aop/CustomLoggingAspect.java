54faaea1de95ce47b4395f7afa152eea09f271a4
Changnam Go <changnam.go@gmail.com>
2025-05-01 19:00:27 +0900

    logback 대신 log4j2 사용

--- src/main/java/com/honsoft/shopmall/aop/CustomLoggingAspect.java
+++ src/main/java/com/honsoft/shopmall/aop/CustomLoggingAspect.java
@@ -4,18 +4,22 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class CustomLoggingAspect {
+	private final Logger logger = LoggerFactory.getLogger(CustomLoggingAspect.class);
+	
 	@Before("execution(* com.honsoft.shopmall..*(..))")
 	public void logBeforeMethodCall(JoinPoint jp) {
-		System.out.println(jp.getSignature()+" >>> 메소드 호출 시작");
+		logger.info(jp.getSignature()+" >>> 메소드 호출 시작");
 	}
 	
 	@After("execution(* com.honsoft.shopmall..*(..))")
 	public void logAfterMethodCall(JoinPoint jp) {
-		System.out.println(jp.getSignature()+" >>> 메소드 호출 완료");
+		logger.info(jp.getSignature()+" >>> 메소드 호출 완료");
 	}
 }
