2b1c6832d6a26117319239d92df99a3c272d93a7
Changnam Go <changnam.go@gmail.com>
2025-05-03 18:30:27 +0900

    1:M 관계에 양방향 적용, 각각에 서로에 대한 정보를 가짐

--- src/main/java/com/honsoft/shopmall/entity/Team.java
+++ src/main/java/com/honsoft/shopmall/entity/Team.java
@@ -1,9 +1,13 @@
 package com.honsoft.shopmall.entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,4 +25,6 @@ public class Team {
 	
 	private String name;
 	
+	@OneToMany(mappedBy = "team", cascade = CascadeType.ALL)
+	private List<Player> players;
 }
