a7f208daab3f910c85791486c0b012ab6052bb43
Changnam Go <changnam.go@gmail.com>
2025-05-10 13:46:53 +0900

    BindingResult 가 있으면 springboot 는 validation 에러에 대해서 exception 을 throw 하지 않는다. user 가 수동으로 처리하도록 기대함

--- src/main/java/com/honsoft/shopmall/restcontroller/ReactController.java
+++ src/main/java/com/honsoft/shopmall/restcontroller/ReactController.java
@@ -1,22 +1,33 @@
 package com.honsoft.shopmall.restcontroller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.honsoft.shopmall.dto.BoardFormDto;
+import com.honsoft.shopmall.dto.MemberFormDto;
 import com.honsoft.shopmall.response.ResponseHandler;
 
 @RestController
 @RequestMapping("/api")
 public class ReactController {
-
+	private static Logger logger = LoggerFactory.getLogger(ReactController.class);
+	
 	@CrossOrigin(origins = "http://localhost:3000") // ✅ Allows only this frontend
 	@PostMapping("/register")
-	public ResponseEntity<Object> registerUser(){
+	public ResponseEntity<Object> registerUser(@Validated @RequestBody MemberFormDto member, BindingResult bindingResult){
+		logger.info("member: "+member.toString());
+		if(bindingResult.hasErrors()) {
+			logger.info("bindingerror개수: "+bindingResult.getErrorCount());
+		}
 		return ResponseHandler.responseBuilder("user saved", HttpStatus.OK, BoardFormDto.builder().title("테스트").build());
 		
 	}
