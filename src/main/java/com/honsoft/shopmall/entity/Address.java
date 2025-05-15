977dece63752e156702114e24b6828032e02b53a
Changnam Go <changnam.go@gmail.com>
2025-05-15 09:56:51 +0900

    manytoone 관계에서 반드시 parent 가 존재하도록 강제

--- src/main/java/com/honsoft/shopmall/entity/Address.java
+++ src/main/java/com/honsoft/shopmall/entity/Address.java
@@ -20,8 +20,8 @@ import lombok.ToString;
 
 @Data
 @Entity
-@Builder
-@Table(name = "addresses", uniqueConstraints = @UniqueConstraint(columnNames = { "customer_id", "addressName" }))
+//@Builder
+@Table(name = "addresses")
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString(exclude = {"customer"})
@@ -42,38 +42,9 @@ public class Address {
 
 	private String detailName; // 세부주소
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "customer_id")
+	@ManyToOne(fetch = FetchType.LAZY, optional = false) //반드시 address 에는 customer 가 연결되어야 함
+	@JoinColumn(name = "customer_id", nullable = false) //foreign key 생성시 not null 로 생성
 	@JsonBackReference // bidirectional relationship 에서 serialization 무한루프 방지
 	private Customer customer;
-
-	/*
-	 * public String getDetailName() { return detailName; } public void
-	 * setDetailName(String detailName) { this.detailName = detailName; } public
-	 * String getAddressName() { return addressName; } public void
-	 * setAddressName(String addressName) { this.addressName = addressName; } public
-	 * String getCountry() { return country; } public void setCountry(String
-	 * country) { this.country = country; } public String getZipCode() { return
-	 * zipCode; } public void setZipCode(String zipCode) { this.zipCode = zipCode; }
-	 * 
-	 * @Override public int hashCode() { final int prime = 31; int result = 1;
-	 * result = prime * result + ((addressName == null) ? 0 :
-	 * addressName.hashCode()); result = prime * result + ((country == null) ? 0 :
-	 * country.hashCode()); result = prime * result + ((detailName == null) ? 0 :
-	 * detailName.hashCode()); result = prime * result + ((zipCode == null) ? 0 :
-	 * zipCode.hashCode()); return result; }
-	 * 
-	 * @Override public boolean equals(Object obj) { if (this == obj) return true;
-	 * if (obj == null) return false; if (getClass() != obj.getClass()) return
-	 * false; Address other = (Address) obj; if (addressName == null) { if
-	 * (other.addressName != null) return false; } else if
-	 * (!addressName.equals(other.addressName)) return false; if (country == null) {
-	 * if (other.country != null) return false; } else if
-	 * (!country.equals(other.country)) return false; if (detailName == null) { if
-	 * (other.detailName != null) return false; } else if
-	 * (!detailName.equals(other.detailName)) return false; if (zipCode == null) {
-	 * if (other.zipCode != null) return false; } else if
-	 * (!zipCode.equals(other.zipCode)) return false; return true; }
-	 */
 
 }
