c923a9a528954b599eec754563569ff0927123f0
Changnam Go <changnam.go@gmail.com>
2025-05-18 21:35:13 +0900

    customerUserDetailsService 를 이용하는 authentication manager bean 을 등록함 (global authentication manager 와는 다른 bean이며, global 은 여전히 두개의 userdetailsservice 빈이 존재하여 사용할수 없는 상태임), 수동 등록된 bean 은 다른데서 주입받아서 사용가능하므로 정상으로 기동됨

--- src/main/java/com/honsoft/shopmall/config/SecurityConfig.java
+++ src/main/java/com/honsoft/shopmall/config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package com.honsoft.shopmall.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
@@ -115,19 +118,20 @@ public class SecurityConfig {
         return http.build();
     }
 
-//    @Bean
-//    public AuthenticationManager authenticationManager(@Qualifier("customerUserDetailsService") UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
-//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-//        authenticationProvider.setUserDetailsService(userDetailsService);
-//        authenticationProvider.setPasswordEncoder(passwordEncoder);
-//
-//        return new ProviderManager(authenticationProvider);
-//    }
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-    	return config.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(@Qualifier("customerUserDetailsService") UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(authenticationProvider);
     }
-	
+    
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+//    	return config.getAuthenticationManager();
+//    }
+//	
 //	List<Permission> permissions = permissionRepository.findAll();
 //
 //    ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry authorize = http
