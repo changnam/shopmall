63770ec7a026c4abc7266d1e63fec773ba7f8154
Changnam Go <changnam.go@gmail.com>
2025-05-20 08:16:40 +0900

    cors 설정 방법 변경

--- src/main/java/com/honsoft/shopmall/config/WebConfig.java
+++ src/main/java/com/honsoft/shopmall/config/WebConfig.java
@@ -15,6 +15,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -100,17 +101,21 @@ public class WebConfig implements WebMvcConfigurer {
         return new HiddenHttpMethodFilter();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-    	CorsConfiguration config = new CorsConfiguration();
-    	config.setAllowedOrigins(List.of("http://localhost:3000", "https://myapp.com"));
-    	config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","HEAD","OPTIONS"));
-    	config.setAllowedHeaders(List.of("Authorization","Cache-Control","Content-Type"));
-    	config.setAllowCredentials(true);
-
-    	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    	source.registerCorsConfiguration("/**", config);
-    	return source;
-    }
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//    	CorsConfiguration config = new CorsConfiguration();
+//    	config.setAllowedOrigins(List.of("http://localhost:3000", "https://myapp.com"));
+//    	config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","HEAD","OPTIONS"));
+//    	config.setAllowedHeaders(List.of("Authorization","Cache-Control","Content-Type"));
+//    	config.setAllowCredentials(true);
+//
+//    	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    	source.registerCorsConfiguration("/**", config);
+//    	return source;
+//    }
     
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+    	registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*").allowCredentials(true);
+    }
 }
