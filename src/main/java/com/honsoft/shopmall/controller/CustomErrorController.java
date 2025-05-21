6ee218a81747e7255a67140eb4bda23a1b8f1206
Changnam Go <changnam.go@gmail.com>
2025-05-21 21:12:10 +0900

    thymeleaf 에러는 mvc controller 가 모두 끝나후 발생하는 것이어서 globaladvice 에서 catch 못함. custom error controller 를 등록한다

--- src/main/java/com/honsoft/shopmall/controller/CustomErrorController.java
+++ src/main/java/com/honsoft/shopmall/controller/CustomErrorController.java
@@ -1,36 +1,47 @@
 package com.honsoft.shopmall.controller;
 
-import java.util.Enumeration;
-
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 
-//@Controller
+@Controller
 public class CustomErrorController implements ErrorController {
+//
+//    @RequestMapping("/error")
+//    public String handleError(HttpServletRequest request, Model m) {
+//        // 여기에 Thymeleaf 에러 포함 모든 에러가 온다
+//        Object status = request.getAttribute("jakarta.servlet.error.status_code");
+//        Object exception = request.getAttribute("jakarta.servlet.error.exception");
+////        Enumeration<String> attrNames = request.getAttributeNames();
+////        while(attrNames.hasMoreElements()) {
+////        	System.out.println("attrName: "+attrNames.nextElement());
+////        }
+//        //System.out.println("에러 상태 코드: " + status);
+//        //System.out.println("예외 정보: " + exception);
+//
+//        // 필요하면 상태코드별로 다른 뷰 리턴 가능
+////        return "error"; // templates/error.html 보여주기
+//        
+//        m.addAttribute("status", status.toString());
+//        m.addAttribute("error", "error");
+//        m.addAttribute("message", ((Exception)exception).getMessage());
+//		return "errorPage";
+//    }
+    
+        @RequestMapping("/error")
+        public ModelAndView handleError(HttpServletRequest request) {
+            Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+            Object exception = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
 
-    @RequestMapping("/error")
-    public String handleError(HttpServletRequest request, Model m) {
-        // 여기에 Thymeleaf 에러 포함 모든 에러가 온다
-        Object status = request.getAttribute("jakarta.servlet.error.status_code");
-        Object exception = request.getAttribute("jakarta.servlet.error.exception");
-//        Enumeration<String> attrNames = request.getAttributeNames();
-//        while(attrNames.hasMoreElements()) {
-//        	System.out.println("attrName: "+attrNames.nextElement());
-//        }
-        //System.out.println("에러 상태 코드: " + status);
-        //System.out.println("예외 정보: " + exception);
+            ModelAndView modelAndView = new ModelAndView("errorCommon");
+            modelAndView.addObject("status", status);
+            modelAndView.addObject("exception", exception);
+            return modelAndView;
+        }
 
-        // 필요하면 상태코드별로 다른 뷰 리턴 가능
-//        return "error"; // templates/error.html 보여주기
-        
-        m.addAttribute("status", status.toString());
-        m.addAttribute("error", "error");
-        m.addAttribute("message", ((Exception)exception).getMessage());
-		return "errorPage";
-    }
 }
