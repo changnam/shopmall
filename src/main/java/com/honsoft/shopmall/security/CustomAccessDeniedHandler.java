11648956564c421de322efbbff464a642e059b5f
Changnam Go <changnam.go@gmail.com>
2025-05-22 22:47:58 +0900

    api 403 오류에 대해서 상세 오류 내용을 로깅 및 client 에 전달

--- src/main/java/com/honsoft/shopmall/security/CustomAccessDeniedHandler.java
+++ src/main/java/com/honsoft/shopmall/security/CustomAccessDeniedHandler.java
@@ -4,8 +4,12 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
@@ -16,16 +20,25 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+	private static final Logger logger = LoggerFactory.getLogger(CustomAccessDeniedHandler.class);
 
 	private final ObjectMapper objectMapper = new ObjectMapper();
 
 	@Override
-	public void handle(HttpServletRequest request, HttpServletResponse response,
-			AccessDeniedException accessDeniedException) throws IOException {
+	public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException ex)
+			throws IOException {
 
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		String logMessage = String.format("user: %s, authorities: %s, %s for %s",
+				auth != null ? auth.getName() : "anonymous", auth != null ? auth.getAuthorities() : "none",
+				ex.getMessage(), request.getRequestURI());
+
+//		logger.info("user: {}, authorities: {}, {} for {}",auth.getName(),auth.getAuthorities(),ex.getMessage(),request.getRequestURI());
+		logger.info(logMessage);
 		if (!response.isCommitted()) {
 			Map<String, Object> body = new HashMap<>();
-			body.put("message", accessDeniedException.getMessage());
+			body.put("message", logMessage);
 			body.put("error", "ACCESS_DENIED");
 			body.put("data", null);
 
