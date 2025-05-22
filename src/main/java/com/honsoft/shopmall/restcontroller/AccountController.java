998192fad25d62f2965f252717a0b913ccadf42f
Changnam Go <changnam.go@gmail.com>
2025-05-22 21:46:29 +0900

    account 전체 조회시 기본 page 값으로 데이터 리턴

--- src/main/java/com/honsoft/shopmall/restcontroller/AccountController.java
+++ src/main/java/com/honsoft/shopmall/restcontroller/AccountController.java
@@ -2,12 +2,17 @@ package com.honsoft.shopmall.restcontroller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.honsoft.shopmall.dto.AccountDto;
@@ -17,21 +22,33 @@ import com.honsoft.shopmall.service.AccountService;
 @RestController
 @RequestMapping("/api/v1/accounts")
 public class AccountController {
-	
+
 	private final AccountService accountService;
+
 	public AccountController(AccountService accountService) {
 		this.accountService = accountService;
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Object> createAccount(@RequestBody AccountDto accountDto){
+	public ResponseEntity<Object> createAccount(@RequestBody AccountDto accountDto) {
 		AccountDto created = accountService.createAccount(accountDto);
 		return ResponseHandler.responseBuilder("account created", HttpStatus.OK, created);
 	}
 
 	@GetMapping
-	public ResponseEntity<Object> getAllAccounts(){
-		List<AccountDto> dtoList = accountService.getAllAccounts();
-		return ResponseHandler.responseBuilder("accounts selected", HttpStatus.OK, dtoList);
+	public ResponseEntity<Object> getAllAccounts(@RequestParam(name = "page", defaultValue = "0") int page,
+			@RequestParam(name = "size", defaultValue = "10") int size,
+			@RequestParam(name = "sort", defaultValue = "email,asc") String sort) {
+
+		String[] sortParts = sort.split(",");
+		String sortField = sortParts[0];
+		String sortDir = sortParts.length > 1 ? sortParts[1] : "asc";
+
+		Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.fromString(sortDir), sortField));
+		// List<AccountDto> dtoList = accountService.getAllAccounts();
+		Page<AccountDto> dtoPage = accountService.getPageAccounts(pageable);
+
+//		List<AccountDto> dtoList = accountService.getAllAccounts();
+		return ResponseHandler.responseBuilder("accounts selected", HttpStatus.OK, dtoPage);
 	}
 }
